7e6d066fba2ff26d570572f342e8f4c2c840ecf0
hakkmmmaj-cell <hakkmmmaj@gmail.com>
2026-06-18 14:06:49 +0300

    Delete mp3.py

--- mp3.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import *
-import os
-
-def convert_to_mp3(video_path, output_path):
-    video = VideoFileClip(video_path)
-    video.audio.write_audiofile(output_path)
-    video.close()
