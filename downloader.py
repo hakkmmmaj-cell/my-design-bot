b1ab42ce28216f699fc7b4c2f9cd5bc7e2a04852
hakkmmmaj-cell <hakkmmmaj@gmail.com>
2026-06-18 14:12:04 +0300

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,7 @@
+import yt_dlp
+
+def download_video(url, output_path):
+    ydl_opts = {'format': 'best', 'outtmpl': output_path}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    print("تم التحميل بنجاح!")
