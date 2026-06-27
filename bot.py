837cd7ff56b7aae8e9a35afbe0667c97c3981694
hakkmmmaj-cell <hakkmmmaj@gmail.com>
2026-06-27 13:11:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
-import telebot
-from telebot import TeleBot
 import os
+import telebot
 
-# الآن يمكنك استخدام TeleBot
-TOKEN = os.environ.get('8609342978:AAGdvzdShxfhyEoOM_ob1k9_fT9mZs-QPOw')
-bot = TeleBot(TOKEN)
+# جرب وضع التوكن مباشرة هنا بين علامات التنصيص لتختبر التشغيل
+TOKEN = "8609342978:AAGdvzdShxfhyEoOM_ob1k9_fT9mZs-QPOw" 
+
+# إذا كنت لا تزال تريد استخدام المتغيرات، تأكد أنها موجودة كما في الخطوة 1
+# TOKEN = os.environ.get('BOT_TOKEN') 
+
+bot = telebot.TeleBot(TOKEN)
