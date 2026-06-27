1c75ca04eaa8dd8b2b8f7e7fcb1032fcb5d011a5
hakkmmmaj-cell <hakkmmmaj@gmail.com>
2026-06-27 12:47:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
+import telebot
+from telebot import TeleBot
 import os
+
+# الآن يمكنك استخدام TeleBot
 TOKEN = os.environ.get('8609342978:AAGdvzdShxfhyEoOM_ob1k9_fT9mZs-QPOw')
 bot = TeleBot(TOKEN)
